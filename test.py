19f818ba1a1626ea540129472796c8d1ef6e6b6b
Harpreet <jonlink01@gmail.com>
2022-05-18 14:22:46 +0530

    final script

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-print("hello")
+from importlib.resources import path
 from netmiko import ConnectHandler
 net_connect = ConnectHandler(device_type="cisco_ios", host="sandbox-iosxe-latest-1.cisco.com", 
 username="developer", password="C1sco12345")
 net_connect.find_prompt()
-output= net_connect.send_command("show ip int br")
+output= net_connect.send_command("show run | in hostname")
+output_backup = net_connect.send_command("show run")
+file_name=(len(output))
+output=(output[9:file_name])
 print(output)
+file = open("/Users/harpreetsingh/Desktop/python_study/study/"+output+".txt", "w")
+file.write(output_backup)
+file.close()
+
+ 
